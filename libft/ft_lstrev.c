22534bafabbfad8175c7f631016e05486c57377e
vyhuholl <olga-p-98@mail.ru>
2019-04-30 20:49:15 +0300

    Add files via upload

---
+++ libft/ft_lstrev.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sghezn <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/30 18:11:03 by sghezn            #+#    #+#             */
+/*   Updated: 2019/04/30 18:12:26 by sghezn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstrev(t_list **lst)
+{
+	t_list	*prev;
+	t_list	*curr;
+	t_list	*next;
+
+	prev = NULL;
+	curr = *lst;
+	next = NULL;
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*lst = prev;
+}
